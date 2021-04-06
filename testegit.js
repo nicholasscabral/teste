5532ad0711d1f75654438e2f0d7f1d6a816056ee
nicholasscabral <nicholasscabral@gmail.com>
2021-04-06 20:49:33 -0300

    adicionando informacoes

--- testegit.js
+++ testegit.js
@@ -1,5 +1,10 @@
 /*
   git init : iniciar um repositorio local
   
+  git add : adiciona as alterações para o "stage"
+
+  git commit -m 'mensagem do commit'
+
+  git push : envia as alterações pro repositorio remoto (github)
 
 */
